4ac511cdbc81ec77914eb94d0bc8f5a59a322cd5
vLaauTy <96405301+vLaauTy@users.noreply.github.com>
2022-11-17 22:20:57 -0300

    probando deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import data from "./data.js"
+import path from 'path';
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
@@ -27,6 +27,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/seed', seedRouter)
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
